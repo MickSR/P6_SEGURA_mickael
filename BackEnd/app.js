29aa2f2ec3b25cf2dcf580e5d3762570fb0044c3
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-13 10:56:25 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,4 +1,8 @@
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
+
+module.exports = app;
 
 mongoose.connect('mongodb+srv://elsequiel:Narutomic1@cluster0.7iwy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
