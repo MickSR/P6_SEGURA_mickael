a95f74839465646d392cfe30d91f4e774a867a08
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-13 10:44:52 +0100

    commit

---
+++ BackEnd/app.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://elsequiel:Narutomic1@cluster0.7iwy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
