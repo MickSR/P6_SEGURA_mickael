8465f2fd310fd9fcbbc4119bfc9bf70f4ec3bb3c
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-18 10:24:13 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require("dotenv").config(); //gestion des variables d'environnement
 
 //express.json()) pour analyser le corps de la requête (bodyparser) en tant qu'objet JSON
 //extended : true pour analyser des objets imbriqués (chaînes ou tableaux)
@@ -12,7 +13,7 @@ app.use((req, res) => {
   res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
-mongoose.connect('mongodb+srv://elsequiel:malabarbak@cluster0.7iwy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(process.env.SECRET_DB,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
