36dd80962d4b23337308e5a53c2dd901a3a26eba
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-18 16:06:10 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,27 +1,37 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const dotenv = require("dotenv").config(); //gestion des variables d'environnement
-const helmet = require("helmet"); //aide à sécuriser mes applications Express en définissant divers en-têtes HTTP
-const path = require("path"); // gestion des chemins de fichiers
-const userRoutes = require("./routes/user");
+const helmet = require("helmet");
+const path = require('path');
+const app = express();
 
-app.use(express.json());//express.json()) pour analyser le corps de la requête (bodyparser) en tant qu'objet JSON
+const userRoutes = require('./routes/user');
 
+
+require("dotenv").config();
 mongoose.connect(process.env.SECRET_DB,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(helmet());
+
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use("/api/auth", userRoutes);
+// Express app secure toolset
+app.use(helmet());
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
