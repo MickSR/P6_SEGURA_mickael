2c3ab85bad873a34222dd17ec5a97cc4429f215d
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-20 10:03:39 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -15,14 +15,11 @@ mongoose.connect(process.env.SECRET_DB,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-  app.use(express.json());
-  app.use(
-    express.urlencoded({
-      extended: true,
-    })
+  app.use(express.json());//analyse du corps de la requête en tant qu'objets JSON (utilisé pour les requêtes POST)
+  app.use(express.urlencoded({extended: true,})//méthode intégrée à express pour reconnaître l'objet de requête entrant sous forme de chaînes ou de tableaux
   );
 
-app.use((req, res, next) => {
+app.use((req, res, next) => { //passer la sécurité CORS , on autorise toutes les origines à accéder à notre API
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./app");
 
-const normalizePort = val => {
+const normalizePort = val => { // renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne;
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -15,7 +15,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-const errorHandler = error => {
+const errorHandler = error => { //recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur;
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -38,7 +38,7 @@ const errorHandler = error => {
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
-server.on('listening', () => {
+server.on('listening', () => { //un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
   console.log('Listening on ' + bind);
