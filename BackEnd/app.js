ec2c2b501980628bb68288944d51804ef078ba70
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-26 14:29:51 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => { //passer la sécurité CORS , on autorise toutes l
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 });
 
