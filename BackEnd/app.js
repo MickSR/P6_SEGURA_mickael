7915e3b89d27a3e3c4ccb86dfdb58ebfc0c9c081
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-26 14:56:58 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,6 +10,7 @@ require("dotenv").config();//charge les variables d'environnement d'un .envfichi
 app.use((req, res, next) => { //passer la sécurité CORS , on autorise toutes les origines à accéder à notre API
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
