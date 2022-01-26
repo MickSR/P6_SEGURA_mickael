eb04f76d9b063a74c77d7bcb2dd8ba4ee2d47b67
MickSR <91461892+MickSR@users.noreply.github.com>
2022-01-26 14:37:17 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -8,8 +8,8 @@ const userRoutes = require('./routes/user');
 require("dotenv").config();//charge les variables d'environnement d'un .envfichier dans process.env
 
 app.use((req, res, next) => { //passer la sécurité CORS , on autorise toutes les origines à accéder à notre API
-  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
