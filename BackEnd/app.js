f8d9927dcca4295f215f7aa53b83a8dbb86e940c
MickSR <91461892+MickSR@users.noreply.github.com>
2022-02-01 10:33:41 +0100

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const helmet = require("helmet");
-const path = require('path');
+const path = require("path");
+
 const app = express();
 const sauceRoutes = require("./routes/sauce");
-const userRoutes = require('./routes/user');
-require("dotenv").config();//charge les variables d'environnement d'un .envfichier dans process.env
+const userRoutes = require("./routes/user");
+require("dotenv").config(); // charge les variables d'environnement d'un .envfichier dans process.env
 
-app.use((req, res, next) => { //passer la sécurité CORS , on autorise toutes les origines à accéder à notre API
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+app.use((req, res, next) => {
+  // passer la sécurité CORS , on autorise toutes les origines à accéder à notre API
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-mongoose.connect(process.env.SECRET_DB,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(process.env.SECRET_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+app.use(express.json()); // analyse du corps de la requête en tant qu'objets JSON (utilisé pour les requêtes POST)
+app.use(
+  express.urlencoded({ extended: true }) // méthode intégrée à express pour reconnaître l'objet de requête entrant sous forme de chaînes ou de tableaux
+);
 
-  app.use(express.json());//analyse du corps de la requête en tant qu'objets JSON (utilisé pour les requêtes POST)
-  app.use(express.urlencoded({extended: true,})//méthode intégrée à express pour reconnaître l'objet de requête entrant sous forme de chaînes ou de tableaux
-  );
-
-//Helmet aide à sécuriser vos applications Express en définissant divers en-têtes HTTP
-app.use(helmet({crossOriginResourcePolicy: false,}));
+// Helmet aide à sécuriser vos applications Express en définissant divers en-têtes HTTP
+app.use(helmet({ crossOriginResourcePolicy: false }));
 
 // chemin middleware sauces - utilisateurs - images
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", sauceRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
